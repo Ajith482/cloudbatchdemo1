6df1986aaa7106622c597a4e95daf2406cc00610
ajith <manamajith@gmail.com>
2022-04-11 15:18:03 +0530

    branch commit

---
+++ Employee.java
@@ -0,0 +1,24 @@
+class Employe
+{
+public int eid;
+public String name;
+public double salary;
+Employee(int eid,String name,double salary)
+{
+this.eid=eid;
+this.name=name;
+this.salary=salary;
+}
+public int getEid()
+{
+return eid;
+}
+public String getName()
+{
+return name;
+}
+public double getSalary()
+{
+return salary;
+}
+}
---
+++ TestEmployee.java
@@ -0,0 +1,8 @@
+class TesEmployee
+{
+public static void main(String[] args)
+{
+Employee e=new Employee(101,"abc",123);
+System.out.println(e.getEid());
+System.out.println(e.getName());
+System.out.println(e.getSalary());
